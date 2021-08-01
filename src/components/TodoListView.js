6a3e63a1a4dc748848d933a3076ef750a568adc1
향훈 <hyanghoon@hyanghun-ui-MacBookPro.local>
2021-08-01 22:54:04 +0900

    material design

--- src/components/TodoListView.js
+++ src/components/TodoListView.js
@@ -1,9 +1,18 @@
 import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Checkbox from "@material-ui/core/Checkbox";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 const TodoListView = ({ todoList }) => (
   <div className="todo-list">
     {todoList.map((value) => (
-      <div className="todo-list-row">{value}</div>
+      // <div>{value}</div>
+      <ListItem className="todo-list-row">
+        <Checkbox />
+        <ListItemText>{value}</ListItemText>
+        <DeleteIcon />
+      </ListItem>
     ))}
   </div>
 );
